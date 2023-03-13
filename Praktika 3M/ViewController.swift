4a65b502a10770bf2bdff993ccee8b7c03f2df89
Nurik <N-TNT@MAIL.RU>
2023-03-13 17:20:10 +0600

    Initial Commit

---
+++ Praktika 3M/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Praktika 3M
+//
+//  Created by Нурлан on 13/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
